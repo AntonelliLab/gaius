deeda2fda230a7efd8988fa13d257818f8f83529
Dominic Bennett <dominic.john.bennett@gmail.com>
2020-06-11 16:35:59 +0200

    minor comments

--- other/aotus_test.R
+++ other/aotus_test.R
@@ -34,6 +34,7 @@ for (i in seq_len(nrow(smmry))) {
 # Align
 library(gaius)
 sequence_files = file.path(outdir, list.files(outdir, '.fasta'))
+sequence_files <- sequence_files[!grepl('alignment', sequence_files)]
 alignment_files <- align(sequence_files = sequence_files, method = 'mafft')
 alignment_list <- alignment_read(flpths = alignment_files)
 
@@ -47,14 +48,27 @@ tree$tip.label <- c('Aotus_nancymaaec', 'Aotus_azarai', 'Aotus_griseimembra',
                     'Aotus_trivirgatus', 'Aotus_nigriceps', 'Aotus_vociferans',
                     'Aotus_lemurinus')
 ape::write.tree(tree, file = file.path(outdir, 'tree.tre'))
+matched_names <- name_match(alignment_names = names_from_alignments(alignment_files),
+                            tree_names = names_from_tree(file.path(outdir, 'tree.tre')))
 groups <- groups_get(alignment_files = alignment_files,
-                     tree_file = file.path(outdir, 'tree.tre'))
+                     tree_file = file.path(outdir, 'tree.tre'),
+                     matched_names = matched_names)
 alignment_list <- alignment_read(flpths = alignment_files)
 
-pset(parameter = c('min_nbps', 'min_ntips', 'min_ngenes',
-                   'column_cutoff'),
-     val = c(50, 3, 1, 0.9))
+# Future set with pset?
+#pset(parameter = c('min_nbps', 'min_ntips', 'min_ngenes',
+#                   'column_cutoff'),
+#     val = c(50, 3, 1, 0.9))
 supermatrices <- supermatrices_get(groups = groups, min_ntips = 3,
+                                   column_cutoff = 0.1, tip_cutoff = 0.1,
+                                   min_nbps = 50,
+                                   min_ngenes = 1,
+                                   alignment_list = alignment_list)
+# above does not produce any supermatrices: too few groups, specifying groups
+# as NULL puts everything in the same group.
+supermatrices <- supermatrices_get(groups = NULL, min_ntips = 3,
+                                   column_cutoff = 0.1, tip_cutoff = 0.1,
+                                   min_nbps = 50,
                                    min_ngenes = 1,
                                    alignment_list = alignment_list)
 
